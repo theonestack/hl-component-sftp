69fa822e74e1761977bb857032d1b35730c108ab
Guslington <guslington@gmail.com>
2021-03-16 15:31:57 +1100

    update lambda to support chroot using logical directories

--- lambdas/identity_provider/index.py
+++ lambdas/identity_provider/index.py
@@ -5,16 +5,15 @@ import base64
 from botocore.exceptions import ClientError
 
 def handler(event, context):
-    # Begin constructing response
-    response_data = {}
+    resp_data = {}
 
     if 'username' not in event or 'serverId' not in event:
+        print("Incoming username or serverId missing  - Unexpected")
         return response_data
 
     # It is recommended to verify server ID against some value, this template does not verify server ID
-    input_server_id = event['serverId']
     input_username = event['username']
-    print("Username: {}, ServerId: {}".format(input_username, input_server_id))
+    print(f"Username: {input_username}, ServerId: {event['serverId']}")
 
     if 'password' in event:
         input_password = event['password']
@@ -23,66 +22,71 @@ def handler(event, context):
         input_password = ''
 
     # Lookup user's secret which can contain the password or SSH public keys
-    secrets_manager_response = get_secret(f"sftp/{os.environ['ENVIRONMENT_NAME']}/{input_username}")
+    resp = get_secret(f"sftp/{os.environ['ENVIRONMENT_NAME']}/{input_username}")
 
-    if secrets_manager_response != None:
-        user_info_secret_dict = json.loads(secrets_manager_response)
+    if resp != None:
+        resp_dict = json.loads(resp)
     else:
-        # There was an error when calling secrets manager, most likely the username doesn't exist
-        return response_data
+        print("Secrets Manager exception thrown")
+        return {}
 
-    # If we have an incoming password then we are performing password auth and need to validate it agains the Secrets entry
     if input_password != '':
-        # Password Auth Flow - Check if we have a Password Key/Value pair in our Secrets Entry
-        if 'Password' in user_info_secret_dict:
-            response_password = user_info_secret_dict['Password']
-            if response_password != input_password:
-                print("Unable to authenticate user - Incoming password does not match stored")
-                return response_data
+        if 'Password' in resp_dict:
+            resp_password = resp_dict['Password']
+        else:
+            print("Unable to authenticate user - No field match in Secret for password")
+            return {}
+
+        if resp_password != input_password:
+            print("Unable to authenticate user - Incoming password does not match stored")
+            return {}
     else:
         # SSH Public Key Auth Flow - The incoming password was empty so we are trying ssh auth and need to return the public key data if we have it
-        if 'PublicKeys' in user_info_secret_dict:
-            response_data['PublicKeys'] = user_info_secret_dict['PublicKeys']
-
+        if 'PublicKeys' in resp_dict:
+            resp_data['PublicKeys'] = resp_dict['PublicKeys']
+        else:
+            print("Unable to authenticate user - No public keys found")
+            return {}
 
     # If we've got this far then we've either authenticated the user by password or we're using SSH public key auth and
     # we've begun constructing the data response. Check for each key value pair.
     # These are required so set to empty string if missing
-    if 'Role' in user_info_secret_dict:
-        response_data['Role'] = user_info_secret_dict['Role']
+    if 'Role' in resp_dict:
+        resp_data['Role'] = resp_dict['Role']
     else:
         print("No field match for role - Set empty string in response")
-        response_data['Role'] = ''
+        resp_data['Role'] = ''
 
     # These are optional so ignore if not present
-    if 'Policy' in user_info_secret_dict:
-        response_data['Policy'] = user_info_secret_dict['Policy']
+    if 'Policy' in resp_dict:
+        resp_data['Policy'] = resp_dict['Policy']
 
-    if 'HomeDirectory' in user_info_secret_dict:
-        response_data['HomeDirectory'] = user_info_secret_dict['HomeDirectory']
+    if 'HomeDirectoryDetails' in resp_dict:
+        print("HomeDirectoryDetails found - Applying setting for virtual folders")
+        resp_data['HomeDirectoryDetails'] = resp_dict['HomeDirectoryDetails']
+        resp_data['HomeDirectoryType'] = "LOGICAL"
+    elif 'HomeDirectory' in resp_dict:
+        print("HomeDirectory found - Cannot be used with HomeDirectoryDetails")
+        resp_data['HomeDirectory'] = resp_dict['HomeDirectory']
+    else:
+        print("HomeDirectory not found - Defaulting to /")
 
-    return response_data
+    print("Completed Response Data: "+json.dumps(resp_data))
+    return resp_data
 
-# This function calls out to Secrets Manager to lookup user and returns None if there is an error.
-def get_secret(secret_name):
-    # Create a Secrets Manager client
-    client = boto3.session.Session().client(
-        service_name='secretsmanager',
-        region_name=os.environ['AWS_REGION']
-    )
+def get_secret(id):
+    client = boto3.session.Session().client(service_name='secretsmanager')
 
     try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
+        resp = client.get_secret_value(SecretId=id)
         # Decrypts secret using the associated KMS CMK.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
-        if 'SecretString' in get_secret_value_response:
-            secret = get_secret_value_response['SecretString']
-            return secret
+        if 'SecretString' in resp:
+            print("Found Secret String")
+            return resp['SecretString']
         else:
-            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
-            return decoded_binary_secret
-    except ClientError as e:
-        print('Error Talking to SecretsManager: ' + e.response['Error']['Code'])
+            print("Found Binary Secret")
+            return base64.b64decode(resp['SecretBinary'])
+    except ClientError as err:
+        print('Error Talking to SecretsManager: ' + err.response['Error']['Code'] + ', Message: ' + str(err))
         return None
